fb204409de387a6af38e2088bc0100e3ad21dc30
Abejide Ayodele <abejideayodele@gmail.com>
2013-09-13 13:11:09 -0500

    working bob

--- src/main/java/com/vasconsulting/Bob.java
+++ src/main/java/com/vasconsulting/Bob.java
@@ -5,10 +5,11 @@ public class Bob {
   public String talk(String discussion){
     if (discussion.matches("\\s+") || discussion.isEmpty()){
       return "Fine. Be that way!";
-    }else if (discussion == discussion.toUpperCase()){
+    } else if (discussion == discussion.toUpperCase()){
       return "Woah, chill out!";
+    } else if (discussion.endsWith("?")){
+      return "Sure";
     }
-    else return "Sure";
-
+    else return "Whatever.";
   }
 }
